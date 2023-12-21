a6b51db9ca4e428250d5469861907a517aafaa51
Manikandan P <manikandanp@softsquare.biz>
2023-12-21 13:46:16 +0530

    cnage

--- server.py
+++ server.py
@@ -1,12 +1,22 @@
-from flask import Flask, request
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
 
-#port
-port = 8080
+def hello_world(request):
+    name = 'Mani'
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\n"
+    return Response(message)
 
-# Create a Flask server instance
-app = Flask(__name__)
-
-#Home Page
-@app.route('/')
-def home():
-  return {'Done' : 'Success'}
+if __name__ == '__main__':
+    port = 8080
+    with Configurator() as config:
+        print('Runing')
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    print('server::::', server)
+    server.serve_forever()
